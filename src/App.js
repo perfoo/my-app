1e67fef1ae5f5828f6282e79337673feb750e20f
perfoo <perfoo@yahoo.com>
2021-09-15 19:22:12 +0200

    initial

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import * as dayjs from "dayjs";
+import { nanoid } from "nanoid";
+
+//model.id = nanoid();
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Heeeej!</h1>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>YO mothafucka!</p>
+        <p>Today is {dayjs().format("MMMM-DD-YYTHH:mm:ss")}</p>
+        <p>Your unique ID is {nanoid()}</p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://perfoo.github.io/OtkupZG/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          OTKUP ZAGREB
         </a>
       </header>
     </div>
